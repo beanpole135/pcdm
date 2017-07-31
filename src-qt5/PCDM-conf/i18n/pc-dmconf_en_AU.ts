788532608c817b0c14b5cc70a7efef58c821ca3b
ZackaryWelch <welch.zackary@gmail.com>
2017-07-31 16:14:12 -0400

    Added Australian translation files

---
+++ src-qt5/PCDM-conf/i18n/pc-dmconf_en_AU.ts
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Login Manager Settings</source>
+        <translation>Login Manager Settings</translation>
+    </message>
+    <message>
+        <source>Auto login</source>
+        <translation>Auto login</translation>
+    </message>
+    <message>
+        <source>Enable auto login</source>
+        <translation>Enable auto login</translation>
+    </message>
+    <message>
+        <source>Auto login user</source>
+        <translation>Auto login user</translation>
+    </message>
+    <message>
+        <source>Remote login</source>
+        <translation>Remote login</translation>
+    </message>
+    <message>
+        <source>Password Request</source>
+        <translation>Password Request</translation>
+    </message>
+    <message>
+        <source>Please enter the login password for this user</source>
+        <translation>Please enter the login password for this user</translation>
+    </message>
+    <message>
+        <source>Enable Remote Desktop (VNC)</source>
+        <translation>Enable Remote Desktop (VNC)</translation>
+    </message>
+    <message>
+        <source>Misc</source>
+        <translation>Misc</translation>
+    </message>
+    <message>
+        <source>Enable &quot;show password&quot; button</source>
+        <translation>Enable &quot;show password&quot; button</translation>
+    </message>
+    <message>
+        <source>Please enter the remote login password</source>
+        <translation>Please enter the remote login password</translation>
+    </message>
+    <message>
+        <source>Missing Config File</source>
+        <translation>Missing Config File</translation>
+    </message>
+    <message>
+        <source>The PCDM configuration file could not be found: %1</source>
+        <translation>The PCDM configuration file could not be found: %1</translation>
+    </message>
+    <message>
+        <source>This application will now close</source>
+        <translation>This application will now close</translation>
+    </message>
+    <message>
+        <source>Time Delay</source>
+        <translation>Time Delay</translation>
+    </message>
+    <message>
+        <source> Seconds</source>
+        <translation> Seconds</translation>
+    </message>
+    <message>
+        <source>Display available users</source>
+        <translation>Display available users</translation>
+    </message>
+    <message>
+        <source>&amp;Apply</source>
+        <translation>&amp;Apply</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Close</translation>
+    </message>
+    <message>
+        <source>Allow Valid Users with UID under 1000</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Additional Excluded Users</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Allow Stealth Sessions</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>You should run this application as root</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ src-qt5/PCDM/i18n/PCDM_en_AU.ts
@@ -0,0 +1,270 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>FancySwitcher</name>
+    <message>
+        <source>Alt+Left</source>
+        <translation>Alt+Left</translation>
+    </message>
+    <message>
+        <source>Alt+Right</source>
+        <translation>Alt+Right</translation>
+    </message>
+    <message>
+        <source>Alt+Up</source>
+        <translation>Alt+Up</translation>
+    </message>
+    <message>
+        <source>Alt+Down</source>
+        <translation>Alt+Down</translation>
+    </message>
+</context>
+<context>
+    <name>LoginWidget</name>
+    <message>
+        <source>Select</source>
+        <translation>Select</translation>
+    </message>
+    <message>
+        <source>Select an alternate user and clear the password field</source>
+        <translation>Select an alternate user and clear the password field</translation>
+    </message>
+    <message>
+        <source>Select this user</source>
+        <translation>Select this user</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <source>Login to the system with the current user and password</source>
+        <translation>Login to the system with the current user and password</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Password</translation>
+    </message>
+    <message>
+        <source>Hold to view the currently entered password</source>
+        <translation>Hold to view the currently entered password</translation>
+    </message>
+    <message>
+        <source>Login password for the selected user</source>
+        <translation>Login password for the selected user</translation>
+    </message>
+    <message>
+        <source>Available users</source>
+        <translation>Available users</translation>
+    </message>
+    <message>
+        <source>Login to %1</source>
+        <translation>Login to %1</translation>
+    </message>
+    <message>
+        <source>Available desktop environments</source>
+        <translation>Available desktop environments</translation>
+    </message>
+    <message>
+        <source>Please connect your PersonaCrypt device to start login procedures.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stealth Session</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Use a temporary home directory which is deleted on log out)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh available users</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Device encryption key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Device encryption key (personacrypt users only)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PCDMgui</name>
+    <message>
+        <source>Virtual Keyboard</source>
+        <translation>Virtual Keyboard</translation>
+    </message>
+    <message>
+        <source>Locale</source>
+        <translation>Locale</translation>
+    </message>
+    <message>
+        <source>Keyboard Layout</source>
+        <translation>Keyboard Layout</translation>
+    </message>
+    <message>
+        <source>Change Keyboard Layout</source>
+        <translation>Change Keyboard Layout</translation>
+    </message>
+    <message>
+        <source>System</source>
+        <translation>System</translation>
+    </message>
+    <message>
+        <source>Tip: Make sure that caps-lock is turned off.</source>
+        <translation>Tip: Make sure that caps-lock is turned off.</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>Restart</translation>
+    </message>
+    <message>
+        <source>Shut Down</source>
+        <translation>Shut Down</translation>
+    </message>
+    <message>
+        <source>Close PCDM</source>
+        <translation>Close PCDM</translation>
+    </message>
+    <message>
+        <source>Shutdown the computer</source>
+        <translation>Shutdown the computer</translation>
+    </message>
+    <message>
+        <source>Invalid Username/Password</source>
+        <translation>Invalid Username/Password</translation>
+    </message>
+    <message>
+        <source>Username/Password combination is invalid, please try again.</source>
+        <translation>Username/Password combination is invalid, please try again.</translation>
+    </message>
+    <message>
+        <source>System Shutdown</source>
+        <translation>System Shutdown</translation>
+    </message>
+    <message>
+        <source>You are about to shut down the system.</source>
+        <translation>You are about to shut down the system.</translation>
+    </message>
+    <message>
+        <source>Are you sure?</source>
+        <translation>Are you sure?</translation>
+    </message>
+    <message>
+        <source>System Restart</source>
+        <translation>System Restart</translation>
+    </message>
+    <message>
+        <source>You are about to restart the system.</source>
+        <translation>You are about to restart the system.</translation>
+    </message>
+    <message>
+        <source>Change locale (%1)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Change DPI</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>High (4K)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Standard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Low </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh PCDM</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>loginDelay</name>
+    <message>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <source>%v/%m seconds</source>
+        <translation>%v/%m seconds</translation>
+    </message>
+    <message>
+        <source>Cancel Login</source>
+        <translation>Cancel Login</translation>
+    </message>
+    <message>
+        <source>Login Now</source>
+        <translation>Login Now</translation>
+    </message>
+    <message>
+        <source>PCDM Automatic Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>widgetKeyboard</name>
+    <message>
+        <source>Keyboard Settings</source>
+        <translation>Keyboard Settings</translation>
+    </message>
+    <message>
+        <source>Key Layout</source>
+        <translation>Key Layout</translation>
+    </message>
+    <message>
+        <source>Variant</source>
+        <translation>Variant</translation>
+    </message>
+    <message>
+        <source>Keyboard Model</source>
+        <translation>Keyboard Model</translation>
+    </message>
+    <message>
+        <source>( you may type into the space below to test your selected settings. )</source>
+        <translation>( you may type into the space below to test your selected settings. )</translation>
+    </message>
+    <message>
+        <source>&amp;Apply</source>
+        <translation>&amp;Apply</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Close</translation>
+    </message>
+</context>
+<context>
+    <name>widgetLocale</name>
+    <message>
+        <source>Select Locale</source>
+        <translation>Select Locale</translation>
+    </message>
+    <message>
+        <source>Current Locale</source>
+        <translation>Current Locale</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Apply</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+</context>
+</TS>
